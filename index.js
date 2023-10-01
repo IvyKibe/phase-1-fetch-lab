04b5b23292d1a0ec2847722682443aa75ba8854a
ivykibe <maleeivy@gmail.com>
2023-10-02 00:12:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// Functions are to fetch the books from the API 
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  const url = 'https://anapioficeandfire.com/api/books';
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+    .catch(error => console.error('Error:', error));
 }
 
+// Functions to render the books on webpage
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
